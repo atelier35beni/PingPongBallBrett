c62959bba42fa633cdaa1a6c785352d393e084e6
atelier35beni <35612173+atelier35beni@users.noreply.github.com>
2018-01-19 21:34:05 +0100

    Create RGB.h

---
+++ RGB.h
@@ -0,0 +1,17 @@
+struct RGB {
+  byte r;
+  byte g;
+  byte b;
+  
+};
+
+RGB white = { 255, 255, 255};
+RGB red = {255, 0, 0};
+RGB off = {0, 0, 0};
+RGB green = {0, 255, 0};
+RGB blue = {0, 0, 255};
+RGB pink = {255, 0, 255};
+RGB regen = {150, 150, 255};
+RGB blitz = {240, 240, 255};
+RGB sonne = {255, 255, 50};
+RGB nebel = {90, 70, 100}
